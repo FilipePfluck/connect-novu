79b6eba5cbfc42ca2ab54890d0724d6c6873437a
Filipe Pfluck <filipepfluckdev@gmail.com>
2023-05-04 09:35:09 -0300

    adding search input component

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,13 +1,9 @@
-import { CVAExampleComponent } from "@/components/example/CVA";
+import { Searchbar } from '@/components/Searchbar'
 
 export default function Home() {
   return (
-    <main
-      className='flex min-h-screen flex-col items-center justify-between p-24 bg-zinc-500'
-    >
-      <p>Hello world</p>
-      <CVAExampleComponent/>
-      <CVAExampleComponent intent="secondary"/>
+    <main className="flex min-h-screen flex-col items-center justify-between p-24 bg-zinc-500">
+      <Searchbar />
     </main>
   )
 }
